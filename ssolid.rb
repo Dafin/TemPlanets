cf74e099a16a2d4303ccdc43971a91d124c820f1
Dafin Aziz <Dafin@users.noreply.github.com>
2016-05-26 17:57:51 +0100

    Create solid.rb

---
+++ ssolid.rb
@@ -0,0 +1,11 @@
+
+require './planet'
+
+class Solid < Planet
+    def atmosphere
+        "solid"
+    end
+    def inhabitants
+        "perhaps -- if there's the right nitrogen / oxygen mix"
+    end
+end
